8a4af006636dd76ccc398b2b9a266b13afd79fbb
khalidlotfi <khalidlotfi2005@gmail.com>
2024-02-12 23:03:29 +0100

    done - PROJECT 5
    Signed-off-by: khalidlotfi <khalidlotfi2005@gmail.com>

--- toDoList__Master/app.js
+++ toDoList__Master/app.js
@@ -1,9 +1,41 @@
 import Queue from "./queue.js";
 let list = new Queue(9);
 
-let add = document.querySelector(".add__btn");
-let remove = document.querySelector(".remove__btn");
-add.addEventListener("click", () => {
+document.addEventListener("DOMContentLoaded", () => {
+  let add = document.querySelector(".add__btn");
+  let remove = document.querySelector(".remove__btn");
+
+  add.addEventListener("click", addTask);
+  remove.addEventListener("click", removeTask);
+
+  document.addEventListener("click", (e) => {
+    if (e.target.classList.contains("done__btn")) {
+      e.target.parentElement.classList.toggle("completed");
+    }
+  });
+});
+
+// text HTML
+const addTaskToHtml = (task) => {
+  let items = document.querySelector("main ol");
+  items.insertAdjacentHTML(
+    "beforeend",
+    `<li class="item">${task}<button class="done__btn"><i class="fa-solid fa-check"></i></button></li>`
+  );
+};
+
+const removeTaskFromHtml = (item) => {
+  let ol = Array.from(document.querySelectorAll("ol li"));
+  ol.forEach((element) => {
+    if (element.textContent === item) {
+      element.remove();
+    }
+  });
+};
+
+// add remove check Task functions
+
+const addTask = () => {
   let input = document.querySelector("#task");
   let task = input.value.trim();
 
@@ -15,29 +47,8 @@ add.addEventListener("click", () => {
   } else {
     alert("Please enter a task");
   }
-});
-
-remove.addEventListener("click", (item) => {
-  let ol = Array.from(document.querySelectorAll("ol li"));
-  console.log(ol);
-  ol.forEach((element) => {
-    if (element.textContent === item) {
-      element.remove();
-    }
-  });
-});
-
-// text HTML
-const addTaskToHtml = (task) => {
-  let items = document.querySelector("main ol");
-  items.insertAdjacentHTML(
-    "beforeend",
-    `<li class="item">${task}</li><button class="done__btn"><i class="fa-solid fa-check"></i></button>`
-  );
 };
 
-// add remove check Task functions
-document.querySelector(".done__btn");
 const removeTask = () => {
   let item = list.dequeue();
   console.log(`${item} has has been deleted`);
